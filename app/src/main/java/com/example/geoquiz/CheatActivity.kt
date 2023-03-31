c89020ecc2bc07162dda9c0511353535ac4909a9
Ali K <karimiafsharali@gmail.com>
2023-03-30 23:35:47 -0500

    add companion object to cheat activity

--- app/src/main/java/com/example/geoquiz/CheatActivity.kt
+++ app/src/main/java/com/example/geoquiz/CheatActivity.kt
@@ -1,8 +1,12 @@
 package com.example.geoquiz
 
+import android.content.Context
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.geoquiz.databinding.ActivityCheatBinding
+
+const val EXTRA_ANSWER_IS_TRUE = "Cheated"
 
 class CheatActivity : AppCompatActivity() {
     private lateinit var binding: ActivityCheatBinding
@@ -13,4 +17,11 @@ class CheatActivity : AppCompatActivity() {
         setContentView(binding.root)
     }
 
+    companion object {
+        fun newIntent(packageContext: Context, answerIsTrue: Boolean): Intent {
+            return Intent(packageContext, CheatActivity::class.java).apply {
+                putExtra(EXTRA_ANSWER_IS_TRUE, answerIsTrue)
+            }
+        }
+    }
 }
