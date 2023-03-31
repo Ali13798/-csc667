f16756ba119c288b7cfa4d66b9359d2f2898dc78
Ali K <karimiafsharali@gmail.com>
2023-03-31 00:51:39 -0500

    sending back an intent

--- app/src/main/java/com/example/geoquiz/CheatActivity.kt
+++ app/src/main/java/com/example/geoquiz/CheatActivity.kt
@@ -1,12 +1,13 @@
 package com.example.geoquiz
 
+import android.app.Activity
 import android.content.Context
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.geoquiz.databinding.ActivityCheatBinding
 
-const val EXTRA_ANSWER_IS_TRUE = "Cheated"
+const val EXTRA_ANSWER_SHOWN = "com.example.geoquiz.answer_is_true"
 
 class CheatActivity : AppCompatActivity() {
     private lateinit var binding: ActivityCheatBinding
@@ -15,20 +16,28 @@ class CheatActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityCheatBinding.inflate(layoutInflater)
         setContentView(binding.root)
-       answerIsTrue = intent.getBooleanExtra(EXTRA_ANSWER_IS_TRUE, false)
-       binding.btnShowAnswer.setOnClickListener {
-           val answerText = when {
-               answerIsTrue -> R.string.btn_true
-               else -> R.string.btn_false
-           }
-           binding.tvAnswer.setText(answerText)
-       }
+        answerIsTrue = intent.getBooleanExtra(EXTRA_ANSWER_SHOWN, false)
+        binding.btnShowAnswer.setOnClickListener {
+            val answerText = when {
+                answerIsTrue -> R.string.btn_true
+                else -> R.string.btn_false
+            }
+            binding.tvAnswer.setText(answerText)
+            setAnswerResult(true)
+        }
+    }
+
+    private fun setAnswerResult(isAnswerShown: Boolean) {
+        val data = Intent().apply {
+            putExtra(EXTRA_ANSWER_SHOWN, isAnswerShown)
+        }
+        setResult(Activity.RESULT_OK, data)
     }
 
     companion object {
         fun newIntent(packageContext: Context, answerIsTrue: Boolean): Intent {
             return Intent(packageContext, CheatActivity::class.java).apply {
-                putExtra(EXTRA_ANSWER_IS_TRUE, answerIsTrue)
+                putExtra(EXTRA_ANSWER_SHOWN, answerIsTrue)
             }
         }
     }
