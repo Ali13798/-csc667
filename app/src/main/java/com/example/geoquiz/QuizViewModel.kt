44469c70bb76849c6260bde5bb024a00f2b2ea65
Ali K <karimiafsharali@gmail.com>
2023-03-31 00:54:32 -0500

    tracking cheating in quizViewModel

--- app/src/main/java/com/example/geoquiz/QuizViewModel.kt
+++ app/src/main/java/com/example/geoquiz/QuizViewModel.kt
@@ -5,6 +5,7 @@ import androidx.lifecycle.ViewModel
 
 
 const val CURRENT_INDEX_KEY = "CURRENT_INDEX_KEY"
+const val IS_CHEATER_KEY = "IS_CHEATER_KEY"
 
 class QuizViewModel(private val savedStateHandle: SavedStateHandle) : ViewModel() {
     private var currentIndex: Int
@@ -20,9 +21,14 @@ class QuizViewModel(private val savedStateHandle: SavedStateHandle) : ViewModel(
         get() = questionBank[currentIndex].answer
     val currentQuestionText: Int
         get() = questionBank[currentIndex].textResId
+    var isCheater: Boolean
+        get() = savedStateHandle[IS_CHEATER_KEY] ?: false
+        set(value) = savedStateHandle.set(IS_CHEATER_KEY, value)
+
     fun moveToNext() {
         currentIndex = (currentIndex + 1) % questionBank.size
     }
+
     fun moveToPrev() {
         val n = questionBank.size
         currentIndex = (currentIndex + n - 1) % n
