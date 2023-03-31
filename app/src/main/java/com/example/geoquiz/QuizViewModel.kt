9cedc3dc969d326ba83af6925eeae15fb0d9eee6
Ali K <karimiafsharali@gmail.com>
2023-03-31 00:53:40 -0500

    save data across process death

--- app/src/main/java/com/example/geoquiz/QuizViewModel.kt
+++ app/src/main/java/com/example/geoquiz/QuizViewModel.kt
@@ -1,9 +1,15 @@
 package com.example.geoquiz
 
+import androidx.lifecycle.SavedStateHandle
 import androidx.lifecycle.ViewModel
 
-class QuizViewModel: ViewModel() {
-    private var currentIndex = 0
+
+const val CURRENT_INDEX_KEY = "CURRENT_INDEX_KEY"
+
+class QuizViewModel(private val savedStateHandle: SavedStateHandle) : ViewModel() {
+    private var currentIndex: Int
+        get() = savedStateHandle[CURRENT_INDEX_KEY] ?: 0
+        set(value) = savedStateHandle.set(CURRENT_INDEX_KEY, value)
     private val questionBank = listOf(
         Question(R.string.q_australia, true),
         Question(R.string.q_germany, true),
@@ -13,7 +19,7 @@ class QuizViewModel: ViewModel() {
     val currentQuestionAnswer: Boolean
         get() = questionBank[currentIndex].answer
     val currentQuestionText: Int
-        get()= questionBank[currentIndex].textResId
+        get() = questionBank[currentIndex].textResId
     fun moveToNext() {
         currentIndex = (currentIndex + 1) % questionBank.size
     }
