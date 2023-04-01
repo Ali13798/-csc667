99f498961d29b43e15e96a6f9b0c69311e084e8c
Ali K <karimiafsharali@gmail.com>
2023-04-01 00:12:13 -0500

    implementing a ViewHolder

---
+++ app/src/main/java/us/ak_tech/criminalintent/CrimeListAdapter.kt
@@ -0,0 +1,15 @@
+package us.ak_tech.criminalintent
+
+import androidx.recyclerview.widget.RecyclerView
+import us.ak_tech.criminalintent.databinding.ListItemCrimeBinding
+
+class CrimeHolder(
+    val binding: ListItemCrimeBinding
+) : RecyclerView.ViewHolder(binding.root) {
+
+}
+
+class CrimeListAdapter {
+
+}
+
