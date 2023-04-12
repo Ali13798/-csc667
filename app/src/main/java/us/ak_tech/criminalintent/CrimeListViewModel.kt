aa28daeef712e0d5ad3074545b0f00d0e5b81259
Ali K <karimiafsharali@gmail.com>
2023-04-11 23:49:37 -0500

    add suspend function

--- app/src/main/java/us/ak_tech/criminalintent/CrimeListViewModel.kt
+++ app/src/main/java/us/ak_tech/criminalintent/CrimeListViewModel.kt
@@ -1,12 +1,23 @@
 package us.ak_tech.criminalintent
 
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
 import java.util.*
 
 class CrimeListViewModel : ViewModel() {
     val crimes = mutableListOf<Crime>()
 
     init {
+        viewModelScope.launch {
+            crimes += loadCrimes()
+        }
+    }
+
+    suspend fun loadCrimes(): List<Crime> {
+        // delay(2000)
+        val result = mutableListOf<Crime>()
         for (i in 0..99) {
             val crime = Crime(
                 id = UUID.randomUUID(),
@@ -14,8 +25,8 @@ class CrimeListViewModel : ViewModel() {
                 date = Date(),
                 isSolved = false
             )
-            crimes += crime
+            result += crime
         }
+        return result
     }
-
 }
