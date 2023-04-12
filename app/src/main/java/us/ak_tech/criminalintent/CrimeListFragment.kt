fc451e984ccb9acf224c7d8fea230ffd0a68edcb
Ali K <karimiafsharali@gmail.com>
2023-04-11 23:57:25 -0500

    cleanup logs and old adapter

--- app/src/main/java/us/ak_tech/criminalintent/CrimeListFragment.kt
+++ app/src/main/java/us/ak_tech/criminalintent/CrimeListFragment.kt
@@ -16,8 +16,6 @@ import kotlinx.coroutines.launch
 import us.ak_tech.criminalintent.databinding.FragmentCrimeListBinding
 
 
-private const val TAG = "CrimeListFragment"
-
 class CrimeListFragment : Fragment() {
     private var _binding: FragmentCrimeListBinding? = null
     private val binding
@@ -26,10 +24,6 @@ class CrimeListFragment : Fragment() {
         }
     private val crimeListViewModel: CrimeListViewModel by viewModels()
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        Log.d(TAG, "Total crimes: ${crimeListViewModel.crimes.size}")
-    }
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -38,9 +32,6 @@ class CrimeListFragment : Fragment() {
     ): View? {
         _binding = FragmentCrimeListBinding.inflate(inflater, container, false)
         binding.rcvCrime.layoutManager = LinearLayoutManager(context)
-        val crimes = crimeListViewModel.crimes
-        val adapter = CrimeListAdapter(crimes)
-        binding.rcvCrime.adapter = adapter
         return binding.root
     }
 
