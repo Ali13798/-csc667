d108bd82e2d2d5bdde3c51c3dfdeb14660df45e0
Ali K <karimiafsharali@gmail.com>
2023-04-12 00:09:02 -0500

    add type converters

--- app/src/main/java/us/ak_tech/criminalintent/database/CrimeDatabase.kt
+++ app/src/main/java/us/ak_tech/criminalintent/database/CrimeDatabase.kt
@@ -2,9 +2,11 @@ package us.ak_tech.criminalintent.database
 
 import androidx.room.Database
 import androidx.room.RoomDatabase
+import androidx.room.TypeConverters
 import us.ak_tech.criminalintent.Crime
 
 
 @Database(entities = [Crime::class], version = 1)
+@TypeConverters(CrimeTypeConverters::class)
 abstract class CrimeDatabase : RoomDatabase() {
 }
