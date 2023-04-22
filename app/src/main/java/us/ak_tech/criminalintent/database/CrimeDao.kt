4b417a377edce8a1c108dd0668193b6890d660ba
Ali K <karimiafsharali@gmail.com>
2023-04-22 15:15:15 -0500

    add update query to DAO

--- app/src/main/java/us/ak_tech/criminalintent/database/CrimeDao.kt
+++ app/src/main/java/us/ak_tech/criminalintent/database/CrimeDao.kt
@@ -2,6 +2,7 @@ package us.ak_tech.criminalintent.database
 
 import androidx.room.Dao
 import androidx.room.Query
+import androidx.room.Update
 import us.ak_tech.criminalintent.Crime
 import java.util.UUID
 
@@ -13,4 +14,7 @@ interface CrimeDao {
 
     @Query("SELECT * FROM Crime WHERE id=(:id)")
     suspend fun getCrime(id: UUID): Crime
+
+    @Update
+    suspend fun updateCrime(crime: Crime)
 }
