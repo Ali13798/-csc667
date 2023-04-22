af2a2d08116144a852dc49d6662c601011618bb7
Ali K <karimiafsharali@gmail.com>
2023-04-22 14:05:02 -0500

    pass argument to lambda functions

--- app/src/main/java/us/ak_tech/criminalintent/CrimeListAdapter.kt
+++ app/src/main/java/us/ak_tech/criminalintent/CrimeListAdapter.kt
@@ -5,19 +5,23 @@ import android.view.ViewGroup
 import android.widget.Toast
 import androidx.recyclerview.widget.RecyclerView
 import us.ak_tech.criminalintent.databinding.ListItemCrimeBinding
+import java.util.UUID
 
 class CrimeHolder(val binding: ListItemCrimeBinding) : RecyclerView.ViewHolder(binding.root) {
-    fun bind(crime: Crime, onCrimeClicked: () -> Unit) {
+    fun bind(crime: Crime, onCrimeClicked: (crimeId: UUID) -> Unit) {
         binding.tvCrimeTitle.text = crime.title
         binding.tvCrimeDate.text = crime.date.toString()
         binding.root.setOnClickListener {
-            onCrimeClicked()
+            onCrimeClicked(crime.id)
         }
     }
 
 }
 
-class CrimeListAdapter(private val crimes: List<Crime>, private val onCrimeClicked: () -> Unit) :
+class CrimeListAdapter(
+    private val crimes: List<Crime>,
+    private val onCrimeClicked: (crimeId: UUID) -> Unit
+) :
     RecyclerView.Adapter<CrimeHolder>() {
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): CrimeHolder {
         val inflater = LayoutInflater.from(parent.context)
