e1037434b827047e959a05d6283ade30c814b1a3
Ali K <karimiafsharali@gmail.com>
2023-04-22 14:50:25 -0500

    access CrimeDetailViewModel overrides the default value for the last parameter (a lambda) makes viewModels return an instance of the factory

--- app/src/main/java/us/ak_tech/criminalintent/CrimeDetailFragment.kt
+++ app/src/main/java/us/ak_tech/criminalintent/CrimeDetailFragment.kt
@@ -7,6 +7,7 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.core.widget.doOnTextChanged
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
 import androidx.navigation.fragment.navArgs
 import us.ak_tech.criminalintent.databinding.FragmentCrimeDetailBinding
 import java.util.*
@@ -14,6 +15,9 @@ import java.util.*
 
 class CrimeDetailFragment : Fragment() {
     private val args: CrimeDetailFragmentArgs by navArgs()
+    private val crimeDetailViewModel: CrimeDetailViewModel by viewModels {
+        CrimeDetailViewModelFactory(args.crimeId)
+    }
     private var _binding: FragmentCrimeDetailBinding? = null
     private val binding
         get() = checkNotNull(_binding) {
