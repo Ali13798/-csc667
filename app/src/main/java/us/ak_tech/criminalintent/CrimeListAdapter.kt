6e38810b2e64c31d345aa6a74bf2f57920b9d5ce
Ali K <karimiafsharali@gmail.com>
2023-04-22 13:36:39 -0500

    pass lambda expression allows the creating class to control behavior when clicking a list item.

--- app/src/main/java/us/ak_tech/criminalintent/CrimeListAdapter.kt
+++ app/src/main/java/us/ak_tech/criminalintent/CrimeListAdapter.kt
@@ -7,21 +7,18 @@ import androidx.recyclerview.widget.RecyclerView
 import us.ak_tech.criminalintent.databinding.ListItemCrimeBinding
 
 class CrimeHolder(val binding: ListItemCrimeBinding) : RecyclerView.ViewHolder(binding.root) {
-    fun bind(crime: Crime) {
+    fun bind(crime: Crime, onCrimeClicked: () -> Unit) {
         binding.tvCrimeTitle.text = crime.title
         binding.tvCrimeDate.text = crime.date.toString()
         binding.root.setOnClickListener {
-            Toast.makeText(
-                binding.root.context,
-                "${crime.title} clicked!",
-                Toast.LENGTH_SHORT
-            ).show()
+            onCrimeClicked()
         }
     }
 
 }
 
-class CrimeListAdapter(private val crimes: List<Crime>) : RecyclerView.Adapter<CrimeHolder>() {
+class CrimeListAdapter(private val crimes: List<Crime>, private val onCrimeClicked: () -> Unit) :
+    RecyclerView.Adapter<CrimeHolder>() {
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): CrimeHolder {
         val inflater = LayoutInflater.from(parent.context)
         val binding = ListItemCrimeBinding.inflate(inflater, parent, false)
@@ -33,7 +30,7 @@ class CrimeListAdapter(private val crimes: List<Crime>) : RecyclerView.Adapter<C
     override fun onBindViewHolder(holder: CrimeHolder, position: Int) {
         val crime = crimes[position]
         holder.apply {
-            holder.bind(crime)
+            holder.bind(crime, onCrimeClicked)
         }
     }
 }
