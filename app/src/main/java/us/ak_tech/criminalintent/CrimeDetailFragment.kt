b468c3d8a949bf514de1464e2cb1b48ae5808bd9
Ali K <karimiafsharali@gmail.com>
2023-04-24 16:00:52 -0500

    handling a result. lab8 complete

--- app/src/main/java/us/ak_tech/criminalintent/CrimeDetailFragment.kt
+++ app/src/main/java/us/ak_tech/criminalintent/CrimeDetailFragment.kt
@@ -6,7 +6,6 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.core.widget.doOnTextChanged
 import androidx.fragment.app.Fragment
-import androidx.fragment.app.setFragmentResult
 import androidx.fragment.app.setFragmentResultListener
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.Lifecycle
@@ -16,6 +15,7 @@ import androidx.navigation.fragment.findNavController
 import androidx.navigation.fragment.navArgs
 import kotlinx.coroutines.launch
 import us.ak_tech.criminalintent.databinding.FragmentCrimeDetailBinding
+import java.util.Date
 
 
 class CrimeDetailFragment : Fragment() {
@@ -65,8 +65,9 @@ class CrimeDetailFragment : Fragment() {
             }
         }
 
-        setFragmentResultListener(DatePickerFragment.REQUEST_KEY_DATE) { requestKey, bundle ->
-            TODO()
+        setFragmentResultListener(DatePickerFragment.REQUEST_KEY_DATE) { _, bundle ->
+            val newDate = bundle.getSerializable(DatePickerFragment.BUNDLE_KEY_DATE) as Date
+            crimeDetailViewModel.updateCrime { it.copy(date = newDate) }
         }
     }
 
